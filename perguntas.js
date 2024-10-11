b31357eb3724ac7f51b9d498fb94f1a309b8b939
kauu <133917142+yue845@users.noreply.github.com>
2024-10-11 09:34:48 -0300

    cds

---
+++ perguntas.js
@@ -0,0 +1,122 @@
+criaCartao(
+    'matemática',
+    'Qual é a fórmula de Bhaskara?',
+    '-b±√Δ/2'
+)
+
+criaCartao(
+    'matemática',
+    'Qual a fórmula base de uma Função Afim?',
+    'f(x)=ax+b',
+)
+
+criaCartao(
+    'matemática',
+    'Qual é o cosseno de 60°?',
+    '1/2'
+)
+
+criaCartao(
+    'matemática',
+    'Quais são os 3 primeiros números de π?',
+    '3,14'
+)
+
+criaCartao(
+    'portugues',
+    'Qual a forma de linguagem em que só são usadas imagens?',
+    'Não verbal'
+)
+
+criaCartao(
+    'portugues',
+    'Qual o tipo de texto usado na redação do ENEM?',
+    'Texto dissertativo-argumentativo'
+)
+
+criaCartao(
+    'portugues',
+    'o que é uma charge?',
+    'É um gênero textual jornalístico marcado pelo humor e pela ironia. Utiliza caricaturas para fazer crítica social, política, esportiva etc.'
+)
+
+criaCartao(
+    'portugues',
+    'Qual a diferença entre charge e tirinha?',
+    'É a quantidade de quadros. A charge apresenta apenas um, bastante impactante, já a tirinha traz dois ou mais quadros e uma história a ser acompanhada.'
+)
+
+criaCartao(
+    'geografia',
+    'O que é capitalismo?',
+    'É um sistema econômico que e baseia na propriedade privada, no lucro e no trabalho assalariado.'
+)
+
+criaCartao(
+    'geografia',
+    'Em que período aconteceu a Primeira revolução Industrial?',
+    'De 1760 até 1850'
+)
+
+criaCartao(
+    'geografia',
+    'O que é socialismo?',
+    'O socialismo é uma ideologia política e econômica que busca a propriedade coletiva dos meios de produção e a distribuição equitativa de recursos.'
+)
+
+criaCartao(
+    'geografia',
+    'O que o comunismo defende?',
+    'O comunismo defende a abolicação da propriedade privada, a criação de uma sociedade sem classes e a distribuição igualitária de bens e recursos.'
+)
+
+criaCartao(
+    'historia',
+    'Em que período aconteceu a Ditadura Militar no Brasil?',
+    'De 1964 até 1985'
+)
+
+criaCartao(
+    'historia',
+    'Qual foi o primeiro preidente do Brasil?',
+    'Deodoro da Fonseca'
+)
+
+criaCartao(
+    'historia',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'historia',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'materia',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'materia',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'materia',
+    'pergunta',
+    'resposta'
+)
+
+criaCartao(
+    'materia',
+    'pergunta',
+    'resposta'
+)
+
+//ao todo sao 20 perguntas de memorização do ensino médio
+//matematica, port, geo, hist, bio 4 de cada
