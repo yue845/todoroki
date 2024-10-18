58276f01cefcbac690036441a0b9c780ecfae8ba
kauu <133917142+yue845@users.noreply.github.com>
2024-10-18 08:49:25 -0300

    jgh

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ function criaCartao (categoria, pergunta, resposta){
     <div class="conteudo-cartao">
         <h3>${categoria}</h3>
         <div class="pergunta-cartao">
-            <p>pergunta</p>
+            <p>${pergunta}</p>
         </div>
         <div class="resposta-cartao">
-            <p>resposta</p>
+            <p>${resposta}</p>
         </div>
     </div>
     `
