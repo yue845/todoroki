1532d391bb82a5891c98493fbff9a6d43ac9711e
kauu <133917142+yue845@users.noreply.github.com>
2024-10-25 11:23:38 -0300

    g

--- main.js
+++ main.js
@@ -22,5 +22,5 @@ function criaCartao (categoria, pergunta, resposta){
 
     cartao.addEventListener('click', viraCartao);
 
-    container.appendChild(cartao)
+    container.appendChild(cartao);
 }
