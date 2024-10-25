f3c445ab6c37b6c45eb28f992d082549ef9b0808
kauu <133917142+yue845@users.noreply.github.com>
2024-10-25 12:24:00 -0300

    hgf

--- pagina2.js
+++ pagina2.js
@@ -3,7 +3,7 @@ function criaCartao (categoria, pergunta, resposta){
     let cartao = document.createElement('article');
     cartao.className = 'cartao';
     cartao.innerHTML = `
-    <div class="conteudo-cartao">
+    <div class="conteudo-cartao" onclick="mostrarResposta(this)">
         <h3>${categoria}</h3>
         <div class="pergunta-cartao">
             <p>${pergunta}</p>
