b731c3b32bef0e0ba85fb8e7da79084fb3f1a789
kauu <133917142+yue845@users.noreply.github.com>
2024-11-01 11:48:38 -0300

    dfhg

--- pagina2.js
+++ pagina2.js
@@ -16,12 +16,12 @@ function criaCartao (categoria, pergunta, resposta){
     container.appendChild(cartao);
 }
 
-function mostrarResposta(){
-    const resposta = document.querySelector('.resposta-cartao');
+function mostrarResposta(card){
+    const resposta = card.querySelector('.resposta-cartao');
     if(resposta.style.display === 'none'){
-        resposta.style.display === 'block'
+        resposta.style.display = 'block'
     }
     else{
-        resposta.style.display === 'none'
+        resposta.style.display = 'none'
     }
 }
